f83831ac8670d55bdac1b09cba7209d606e5dbbe
Żaneta Jażdżyk <zaneta.jazdzyk@gmail.com>
2020-05-29 18:15:22 +0200

    starting with Gatsby & Firebase, gatsby-firesource added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = (props) => {
+  return (
+    <Layout>
+      {props.data.allBook.nodes.map(node => (
+        <div key={node.id}>
+          <h2>
+            {node.title} - <small>{node.author.name}</small>
+          </h2>
+          <div>{node.summary}</div>
+          <Link to={`/book/${node.id}`}>
+            Join conversation
+            </Link>
+        </div>
+      ))}
+    </Layout>
+  );
+}
+
+export const query = graphql`
+{
+  allBook {
+    nodes {
+      title
+      author {
+        name
+      }
+      id
+      summary
+    }
+  }
+}`;
 
 export default IndexPage
